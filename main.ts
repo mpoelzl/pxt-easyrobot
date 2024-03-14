31a1b7e83924d88126669a26e195d62c4b32434a
mpoelzl <mpoelzl@redeaglesolutions.com>
2024-03-14 08:20:06 +0100

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-forever(function () {
-	
-})
+namespace easyrobot{
+    
+    //% blockId=motorRun block="run %motor at %speed=motorSpeedPicker|\\%||for %value %unit"
+    //% weight=100 blockGap=8
+    //% group="Move"
+    //% motor.fieldEditor="motors"
+    //% motor.fieldOptions.decompileLiterals=1
+    //% expandableArgumentMode=toggle
+    //% help=motors/motor/run
+    export function runMotor(speed: number, value: number = 0, unit: MoveUnit = MoveUnit.MilliSeconds) {
+        motors.largeCD.run(speed, value, unit);
+    }
+}
